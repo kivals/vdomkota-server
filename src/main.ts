1637937acb8a45e02573317adc92de841790cef0
melnik <melnik@bifit.com>
2021-04-01 15:13:55 +0300

    Add helmet and express-rate-limit utilities

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import * as helmet from 'helmet';
+import * as rateLimiter from 'express-rate-limit';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(
+    rateLimiter({
+      windowMs: 60, // 1 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
