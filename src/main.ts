47d4a35a070563de9306f4971578b34961cc7f87
melnik <melnik@bifit.com>
2021-04-22 15:46:50 +0300

    Add Http Filter Exception

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as helmet from 'helmet';
 import * as rateLimiter from 'express-rate-limit';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
       max: 100, // limit each IP to 100 requests per windowMs
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
